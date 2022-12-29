045e1ad4e687bf1495e8857153b7b0a0996d01b4
Techinnovator22 <121611991+Techinnovator22@users.noreply.github.com>
2022-12-29 17:11:14 +0530

    committing files

---
+++ migrations/V20221229171113__objects_DDL.sql
@@ -0,0 +1,7 @@
+-- hack_dec_tbl_demo
+CREATE   TABLE HACKATHON_DB.HACKATHON_SCHEMA.Hack_Dec_Tbl_Demo (firstname varchar(255),
+middlename varchar(255),
+lastname varchar(255),
+salary varchar(255),
+location varchar(255),
+city varchar(255)) COMMENT = 'GITDEMOTEST';
