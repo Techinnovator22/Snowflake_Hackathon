8816792e7edd11867163838693fe9d949d96118e
Techinnovator22 <121611991+Techinnovator22@users.noreply.github.com>
2022-12-29 16:58:33 +0530

    committing files

---
+++ migrations/V20221229165831__objects_DDL.sql
@@ -0,0 +1,9 @@
+-- demo_role_dec
+CREATE  ROLE Demo_Role_Dec  ;
+-- tbl_dec_hack
+CREATE   TABLE DEMO_127.DEMO.Tbl_Dec_Hack (firstname varchar(255),
+lastname varchar(255)) COMMENT = 'TEST';
+-- dec_hack_db_demo
+CREATE   TABLE HACKATHON_DB.HACKATHON_SCHEMA.Dec_Hack_db_demo (firstname varchar(255),
+lastname varchar(255),
+address varchar(255)) COMMENT = 'GITDEMO';
