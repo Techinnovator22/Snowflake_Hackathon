5524b4cca604b338c24e4257e4393e9ee9622938
Techinnovator22 <121611991+Techinnovator22@users.noreply.github.com>
2023-04-11 13:02:51 +0530

    committing files

---
+++ migrations/V20230411130249__bulk_objects_DDL.sql
@@ -0,0 +1,4 @@
+
+CREATE OR REPLACE  TABLE TEST123.SCHEMA123.Department (DeptName varchar (255) Not null, DeptDesc varchar (255) , DeptID integer , Location varchar (255) );
+
+CREATE OR REPLACE  TABLE TEST123.SCHEMA123.Employee (Firstname varchar (255) Not null, Lastname varchar (255) , Role varchar (255) , Salary double );
